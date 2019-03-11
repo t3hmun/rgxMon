590fae3a03129373ae1e261d4cad2d476dbc49b1
t3hmun <git@t3hmun.com>
2019-03-11 08:51:28 +0000

    File monitoring broken because vim remove-renames

--- rgxMon.go
+++ rgxMon.go
@@ -8,12 +8,14 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
-var op = make(chan fsnotify.Event)
+var change = make(chan string)
+var done = make(chan bool)
 var watcher *fsnotify.Watcher
 
 func main() {
 
 	fmt.Printf("RgxMon %s\n", os.Args[1])
+	//TODO: take regex arg
 	var err error
 	watcher, err = fsnotify.NewWatcher()
 	if err != nil {
@@ -21,24 +23,49 @@ func main() {
 	}
 	defer watcher.Close()
 
+	err = watcher.Add(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go watch()
+	go act()
+
+	<-done
+}
+
+func act() {
+	for {
+		select {
+		case c := <-change:
+			fmt.Println("Reacting to %s", c)
+			//TODO: regex the file.
+		case <-done:
+			return
+		}
+	}
 }
 
 func watch() {
 	for {
 		select {
 		case event, ok := <-watcher.Events:
+			fmt.Println(event.Name)
+			fmt.Println(event.Op)
 			if !ok {
+				done <- true
 				return
 			}
 			if event.Op&fsnotify.Write == fsnotify.Write {
 				fmt.Printf(event.Name)
-				op <- event
+				change <- event.Name
 			}
 		case err, ok := <-watcher.Errors:
+			fmt.Println(err)
 			if !ok {
+				done <- true
 				return
 			}
-			fmt.Println(err)
 		}
 	}
 }
